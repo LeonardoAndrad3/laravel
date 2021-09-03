1e913c82876906a39f51844c3fd08ff04a62afc4
leobiriba <leonardoxcontato@gmail.com>
2021-09-03 16:17:01 -0300

    Fazendo alteraçao no webpack e conhecendo a ferramenta

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/', function(){
+Route::get('/carros', function(){
     return view('carros');
 });
-// Route::get('/', function(){
-//     return view('apiCaches');
-// });
+Route::get('/apicaches', function(){
+    return view('apiCaches');
+});
