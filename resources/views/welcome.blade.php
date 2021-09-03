f639a8f34d79b4730d9a7448344c4291de8c5396
leobiriba <leonardoxcontato@gmail.com>
2021-09-03 16:27:18 -0300

    Fazendo alteraçao no webpack e conhecendo a ferramenta

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,7 @@
                 </div>
             @endif
 
-            <!-- <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                     <svg viewBox="0 0 651 192" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-16 w-auto text-gray-700 sm:h-20">
                         <g clip-path="url(#clip0)" fill="#EF3B2D">
@@ -45,7 +45,7 @@
                     </svg>
                 </div>
 
-                <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                <!-- <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
                             <div class="flex items-center">
@@ -106,7 +106,7 @@
                         <div class="flex items-center">
                             <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="-mt-px w-5 h-5 text-gray-400">
                                 <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
-                            </svg>
+                            </svg> -->
 
                             <a href="https://laravel.bigcartel.com" class="ml-1 underline">
                                 Shop
@@ -120,7 +120,7 @@
                                 Sponsor
                             </a>
                         </div>
-                    </div> -->
+                    </div> 
 
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
@@ -129,10 +129,7 @@
             </div>
         </div> 
 
-        <div>
-            <a href="https://gamecarpw.herokuapp.com/apicaches"><button type="button" class="btn btn-primary">ApiCaches</button></a>
-            <a href="https://gamecarpw.herokuapp.com/carros"><button type="button" class="btn btn-primary">Carros</button></a>
-        </div>
+       
 
     </body>
 </html>
